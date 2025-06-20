246dda3953cc6af39172a120bf155d240a5b52ec
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-20 17:48:56 +0530

    Add layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text>My Expo App</Text>
+      </View>
+      <Slot />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>RN Tutorial</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    padding: 20,
+    backgroundColor: "red",
+  },
+  headerText: {
+    color: "white",
+  },
+  footer: {
+    padding: 20,
+    backgroundColor: "blue",
+  },
+  footerText: {
+    color: "white",
+  },
+});
